99620c5f369297c661f057b41c1080b11bbb91f4
Raghani Madhu <raghanimadhu@gmail.com>
2022-10-03 04:03:29 +0530

    Update: Completed Bonus

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ cashGiven = document.querySelector("#cash-given");
 checkButton = document.querySelector("#check-button");
 errorMessage = document.querySelector("#error-message");
 noOfNotesTableCells = document.querySelectorAll(".no-of-notes");
+nextButton = document.querySelector("#next");
+cashGivenLabel = document.querySelector("#cash-given-label");
+table = document.querySelector("table");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+next.addEventListener("click", function checkInput(){
+    if(billAmount.value > 0){
+        hideErrorMessage();
+        nextButton.style.display = "none";
+        cashGivenLabel.style.display = "block";
+        cashGiven.style.display = "block";
+        checkButton.style.display = "block";
+        table.style.display = "block";
+    }else{
+        showErrorMessage("Bill Amount can't be less than 0.");
+    }
+} );
 
 checkButton.addEventListener("click", function processing(){
     hideErrorMessage();
